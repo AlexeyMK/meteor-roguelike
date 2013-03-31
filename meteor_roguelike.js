b834302256615e2a660a2525d8bec9ae1b2460fd
Alexey Komissarouk <alexey@alexeymk.com>
2013-03-31 14:04:24 -0700

    guy moves around

--- meteor_roguelike.js
+++ meteor_roguelike.js
@@ -3,17 +3,17 @@ Entity = new Meteor.Collection("entity");
 if (Meteor.isClient) {
 
   var KEYS_TO_XY_CHANGE = {
-    37: [-1, 0], // left
-    39: [1, 0], // right
-    38: [0, -1], // up
-    40: [0, 1], // down
+    37: {'position.x':-1, 'position.y': 0}, // left
+    39: {'position.x': 1, 'position.y': 0}, // right
+    38: {'position.x': 0, 'position.y': -1}, // u'p
+    40: {'position.x': 0, 'position.y': 1}, // down
   };
 
   $(document).keydown(function(e) {
     //console.log("lets change");
-    change = KEYS_TO_XY_CHANGE[e.keyCode] || [0,0];
+    change = KEYS_TO_XY_CHANGE[e.keyCode] || {};
     Entity.update({_id: "fWmZTarmkfgxQ84b6"},
-      {$inc: {position: change}}
+      {$inc: change}
     );
   });
 
@@ -21,7 +21,7 @@ if (Meteor.isClient) {
     rowSize: function() {return [0,1,2,3,4,5,6,7,8,9];},
     colSize: function() {return [0,1,2,3,4,5,6,7,8,9];},
     value_at_cell: function(x, y) {
-      var entity = Entity.findOne({position: [x, y]});
+      var entity = Entity.findOne({position: {x: x, y: y}});
       if (entity) {
           return entity.display;
       } else {
