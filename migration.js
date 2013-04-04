ae6eda2c44ba3ab8e230f1e59e0704eb7177175a
Alexey Komissarouk <alexey@alexeymk.com>
2013-04-03 23:00:54 -0700

    a couple more migrations

--- migration.js
+++ migration.js
@@ -12,5 +12,20 @@ if (Meteor.isServer) {
       });
       Migrations.insert({name: "addscore"});
     }
+
+    if (!Migrations.findOne({name: "players_back_to_range"})) {
+      Entity.find({$or: [{'position.x': {$gte: BOARDSIZE.x}},
+                         {'position.y': {$gte: BOARDSIZE.y}}]
+      }).forEach(function (entity) {
+        Entity.update({_id: entity._id}, {$set: {
+          position: random_empty_position(),
+        }});
+      });
+      Migrations.insert({name: "players_back_to_range"});
+    }
+    if (!Migrations.findOne({name: "kill_rewards"})) {
+      Entity.remove({reward: {$exists: true}});
+      Migrations.insert({name: "kill_rewards"});
+    }
   });
 }
