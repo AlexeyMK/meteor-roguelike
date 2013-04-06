f26c67f13023686a410a658b585f63a009c6cc40
Alexey Komissarouk <alexey@alexeymk.com>
2013-04-05 17:24:35 -0700

    reactivity

--- rogue_demo.js
+++ rogue_demo.js
@@ -1,9 +1,41 @@
 var BOARDSIZE = {x: 60, y: 15};
 
+BoardObject = new Meteor.Collection("entity");
+
+var random_empty_position = function() {
+  var guess = {
+    x: Math.floor(Math.random() * BOARDSIZE.x),
+    y: Math.floor(Math.random() * BOARDSIZE.y)
+  };
+
+  if (!BoardObject.findOne({position: guess})) {
+    return guess;
+  } else {
+    return random_empty_position();  // try again
+  }
+}
+
+Meteor.startup(function() {
+  var entity_id = BoardObject.insert({
+      position: {x: 2, y:2},
+      display: 'A',
+      display_color: "rgb(0,0,255)",
+      name: 'Alexey',
+      score: 0
+    });
+});
+
 if (Meteor.isClient) {
   Template.grid_world.helpers({
     row: function() { return _.range(BOARDSIZE.y);},
     cell: function() { return _.range(BOARDSIZE.x);},
+    player_at_cell: function(x, y) {
+      var player = BoardObject.findOne({position: {x: x, y: y}});
+      if (player) {
+        return player.display;
+      }
+      return "_";
+    }
   });
 }
 
